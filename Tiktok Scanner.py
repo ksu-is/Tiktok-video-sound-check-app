b969cd02dcf4efc204197169fe363e5fe3a248b4
itsMarc10 <mbineau@students.kennesaw.edu>
2021-12-02 16:49:24 -0500

    Update Tiktok Scanner.py

--- Tiktok Scanner.py
+++ Tiktok Scanner.py
@@ -1,1 +1,16 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
+class TikTokScanner(App):
+    def build(self):
+        self.window = GridLayout()
+        #add widgets to window
+
+        return self.window
+
+if __name__ == "__main__":
+    TikTokScanner().run()
